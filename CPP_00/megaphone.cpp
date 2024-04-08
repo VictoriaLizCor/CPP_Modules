fe1158271cbee276eddd9d858de2a157ce44f4f4
Linda victoria Lizarraga cortes <lilizarr@c2r6s1.42wolfsburg.de>
2024-04-08 16:36:13 +0200

    Done: working on makefile && CPP_00 w

---
+++ CPP_00/megaphone.cpp
@@ -0,0 +1,30 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   megaphone.cpp                                      :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/04/08 13:55:53 by lilizarr          #+#    #+#             */
+/*   Updated: 2024/04/08 14:53:30 by lilizarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+# include <iostream>
+# include <string>
+# include <cstring>
+
+void	ft_str(char *str)
+{
+	std::string s(str);
+}
+
+int	main(int argc, char *argv[])
+{
+	if (argc != 1)
+	{
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		return (0);
+	}
+	return (0);
+}
