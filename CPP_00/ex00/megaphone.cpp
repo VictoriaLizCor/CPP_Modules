0c537626c9de737f90299cd245a5024a2a9c00de
lilizarr <lilizarr@student.42wolfsburg.de>
2024-04-10 16:42:53 +0200

    [@Title] Fix: adding current directory to clean rulew [@Description]
    [@Footer]
    ------------------------------------
    @Autor: Victoria Lizarraga
    ------------------------------------

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 13:55:53 by lilizarr          #+#    #+#             */
-/*   Updated: 2024/04/10 16:14:52 by lilizarr         ###   ########.fr       */
+/*   Updated: 2024/04/10 16:33:16 by lilizarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,13 @@ int	main(int argc, char *argv[])
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 		return (0);
 	}
+	else
+	{
+		for (int i = 1; i < argc; i++)
+		{
+			ft_str(argv[i]);
+		}
+	}
 	return (0);
 }
 
-$3$$f15bc138aca1e76ec6f4cfd0797ec037
