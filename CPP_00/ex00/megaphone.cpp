9fe11684c604c483a0b4e6328b4e2d174751e4d4
lilizarr <lilizarr@student.42wolfsburg.de>
2024-04-11 12:29:06 +0200

    [@Title] Fix: added conversion to uppercase [@Description]
    [@Footer]
    ------------------------------------
    @Autor: Victoria Lizarraga
    ------------------------------------

--- CPP_00/ex00/megaphone.cpp
+++ CPP_00/ex00/megaphone.cpp
@@ -6,15 +6,23 @@
 /*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/08 13:55:53 by lilizarr          #+#    #+#             */
-/*   Updated: 2024/04/11 11:10:53 by lilizarr         ###   ########.fr       */
+/*   Updated: 2024/04/11 12:28:32 by lilizarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 # include <iostream>
 # include <string>
 # include <cstring>
+# include <cctype>
 
+char *str_upper(char *str)
+{
+	for (int i = 0; str[i]; i++)
+		str[i] = std::toupper(str[i]);
+	return (str);
+}
 /**
+** Coding Style and Doxygen Documentation
 ** @brief Print a string with a newline character.
 ** This function takes a char* as input and prints it to the console with a newline character.
 ** It converts the char* to a std::string and then inserts a newline character at the end of the string.
@@ -25,8 +33,7 @@
 void	ft_str(char *s)
 {
 	std::string str(s);
-	str.insert(str.length(), "\n");
-	std::cout << str;
+	std::cout << str_upper((char *)str.c_str());
 }
 
 /**
@@ -40,20 +47,20 @@ void	ft_str(char *s)
  */
 int	main(int argc, char *argv[])
 {
+	std::cout << "\033[0;32m";
 	if (argc != 1)
 	{
-		ft_str(argv[1]);
+		std::cout << str_upper(argv[1]);
 		argv+=2;
 		while (*argv)
 		{
-			std::string str(*argv);
-			str += '\n';
-			std::cout << str;
+			ft_str(*argv);
 			argv++;
 		}
 	}
 	else
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	std::cout << "\033[0m" <<std::endl;
 	return (0);
 }
 
