6477edc20f4985c949e034aa4bd77339e2669fb6
lilizarr <lilizarr@student.42wolfsburg.de>
2024-05-06 16:53:29 +0200

    🆔[cpp01ex00(TICKET:#001)]📝Style: ex00 | Autor: Victoria Lizarraga (@VictoriaLizCor || @lilizarr)

--- CPP_01/ex00/src/main.cpp
+++ CPP_01/ex00/src/main.cpp
@@ -6,7 +6,7 @@
 /*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/03 11:22:17 by lilizarr          #+#    #+#             */
-/*   Updated: 2024/05/06 16:02:50 by lilizarr         ###   ########.fr       */
+/*   Updated: 2024/05/06 16:52:40 by lilizarr         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,7 @@
 
 void compareAddresses(void* addr1, void* addr2, const std::string& name)
 {
-	if (reinterpret_cast<long>(addr1) < reinterpret_cast<long>(addr2))
+	if (long(addr1) < long(addr2))
 		std::cout << "***Zombie " << name << " instance created in Heap memory\n";
 	else
 		std::cout << "***Zombie " << name << " instance created in Stack memory\n";
@@ -51,9 +51,9 @@ int	main(void)
 	Zombie stack1("Stack1");
 	Zombie  *Heap1 = newZombie("Heap1");
 	Heap1->announce();
-	std::cout << "Address of int i_stack: \t" << reinterpret_cast<long>(&i_stack) << std::endl;
-	std::cout << "Address start of HEAP: \t" << reinterpret_cast<long>(i_heap) << std::endl;
-	std::cout << "Address of HEAP Heap1: \t" << reinterpret_cast<long>(Heap1) << std::endl;
+	std::cout << "Address of i_stack: \t" << long(&i_stack) << std::endl;
+	std::cout << "Address start of HEAP: \t" << long(i_heap) << std::endl;
+	std::cout << "Address of HEAP Heap1: \t" << long(Heap1) << std::endl;
 	compareAddresses(&stack1, &i_stack, Zombie::getName(stack1));
 	compareAddresses(Heap1, &i_stack, Zombie::getName(*Heap1));
 	randomChump("Stack");
@@ -61,14 +61,14 @@ int	main(void)
 	delete i_heap;
 	{
 		Zombie stack2("Stk2");
-		std::cout << "Address of int stack1: " << reinterpret_cast<long>(&stack1) << std::endl;
-		std::cout << "Address of int stack2: " << reinterpret_cast<long>(&stack2) << std::endl;
+		std::cout << "Address of int stack1: " << long(&stack1) << std::endl;
+		std::cout << "Address of int stack2: " << long(&stack2) << std::endl;
 		compareAddresses(&stack2, &i_stack, stack2.getName(stack2));
 		{
 			Zombie *Heap2 = 0;
-			std::cout << "Address of HEAP Heap2: " << reinterpret_cast<long>(Heap2) << std::endl;
+			std::cout << "Address of HEAP Heap2: " << long(Heap2) << std::endl;
 			Heap2 = newZombie("Heap2");
-			std::cout << "Address of HEAP Heap2: " << reinterpret_cast<long>(Heap2) << std::endl;
+			std::cout << "Address of HEAP Heap2: " << long(Heap2) << std::endl;
 			Heap2->announce();
 			compareAddresses(Heap1, &i_stack, Heap2->getName(*Heap2));
 			delete Heap2;
