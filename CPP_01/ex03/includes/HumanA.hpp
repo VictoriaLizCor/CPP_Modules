a7d69df43e00ccf3c32d831addea55f15920a558
lilizarr <lilizarr@student.42wolfsburg.de>
2024-05-08 16:34:11 +0200

    🆔[cpp01ex03{003}]📝Fix: Add class HumanA & HumanB
    ~ Autor: Victoria Lizarraga (@VictoriaLizCor / @lilizarr) ~

---
+++ CPP_01/ex03/includes/HumanA.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanA.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 16:33:54 by lilizarr          #+#    #+#             */
+/*   Updated: 2024/05/08 16:34:02 by lilizarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+# ifndef HUMANA_HPP
+#  define HUMANA_HPP
+# include <iostream>
+# include <string>
+# include "Weapon.hpp"
+
+class HumanA
+{
+	private:
+	public:
+};
+
+# endif
---
+++ CPP_01/ex03/includes/HumanB.hpp
@@ -0,0 +1,26 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   HumanB.hpp                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/08 16:32:54 by lilizarr          #+#    #+#             */
+/*   Updated: 2024/05/08 16:33:47 by lilizarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#pragma once
+# ifndef HUMANB_HPP
+#  define HUMANB_HPP
+# include <iostream>
+# include <string>
+# include "Weapon.hpp"
+
+class HumanB
+{
+	private:
+	public:
+};
+
+# endif
