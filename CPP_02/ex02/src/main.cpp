bc64cabb92675cacd39abbf9ce76605b7157dcaf
lilizarr <lilizarr@student.42wolfsburg.de>
2024-05-24 18:01:13 +0200

    🆔 [cpp02ex02{002}] 📝 Fix:
    ~ Autor: Victoria Lizarraga (@VictoriaLizCor / @lilizarr) ~

---
+++ CPP_02/ex02/src/main.cpp
@@ -0,0 +1,37 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   main.cpp                                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lilizarr <lilizarr@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2024/05/23 09:49:47 by lilizarr          #+#    #+#             */
+/*   Updated: 2024/05/24 17:35:11 by lilizarr         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+
+#include "Fixed.hpp"
+#include <iostream>
+#include <bitset>
+#include <iomanip>
+
+#if (DEBUG == 0)
+int main(void)
+{
+	Fixed a;
+	// Fixed const b( Fixed( 5.05f ) * Fixed( 2 ) );
+	// std::cout << a << std::endl;
+	// std::cout << ++a << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << a++ << std::endl;
+	// std::cout << a << std::endl;
+	// std::cout << b << std::endl;
+	// std::cout << Fixed::max( a, b ) << std::endl
+}
+#else
+int main(void)
+{
+	return (0);
+}
+#endif
