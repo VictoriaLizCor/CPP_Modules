99c1087005e378d3568a77d89b25ce5ea869c1c5
lilizarr <lilizarr@student.42wolfsburg.de>
2024-08-25 18:40:25 +0200

    [cpp05] * Fix: 	Updating Intern succesful message
    ~ Autor: Victoria Lizarraga (@VictoriaLizCor / @lilizarr) ~

--- CPP_05/ex03/src/Intern.cpp
+++ CPP_05/ex03/src/Intern.cpp
@@ -64,7 +64,7 @@ AForm* Intern::makeForm(std::string const& formName, std::string const& target)
 	if (it != formCreationMap.end())
 	{
 		form = it->second(target);
-		std::cout << *this << " created Form " << form->getInfo();
+		std::cout << *this << " creates Form " << form->getInfo();
 		nl(3);
 		return (form);
 	}
